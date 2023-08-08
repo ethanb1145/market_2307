09f8e665e384b3b763a9a5dbf9b73691bb6706a9
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-08 10:05:53 -0600

    Finish spec tests and pass them

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Vendor do
   before :each do 
     @vendor = Vendor.new("Rocky Mountain Fresh")
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
   end
 
   describe "#initialize" do 
@@ -28,8 +29,12 @@ RSpec.describe Vendor do
   describe "#stock" do 
     it "stocks a given item" do 
       @vendor.stock(@item1, 30)
-      expect(@vendor.check_stock(@item1)).to eq(30)
-      expect(@vendor.inventory).to eq({@item1 => 30})
+      @vendor.stock(@item1, 25)
+      @vendor.stock(@item2, 12)
+
+      expect(@vendor.check_stock(@item1)).to eq(55)
+      expect(@vendor.check_stock(@item2)).to eq(12)
+      expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
     end
   end
 end
