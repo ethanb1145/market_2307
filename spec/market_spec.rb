70916dfb35b0654d7aebcf4d153cee645bb5fada
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-08 10:40:09 -0600

    Pass spec tests with method

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -35,5 +35,16 @@ RSpec.describe Market do
       expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
     end
   end
+
+  describe "#vendor_names" do 
+    it "shows vendor names in the market" do 
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+      expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
 end
 
