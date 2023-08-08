dca087525358477bedf05fd07580a11baaa88b0f
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-08 11:01:38 -0600

    Move to vendor class instead

--- lib/market.rb
+++ lib/market.rb
@@ -21,10 +21,4 @@ class Market
       vendor.inventory.include?(item)
     end
   end
-
-  def potential_revenue
-    @inventory.sum do |item, quantity|
-      item.price * quantity
-    end
-  end
 end
