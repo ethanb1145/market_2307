67a4ea1a5c75edb6e995148a44fd20f571592840
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-08 10:08:06 -0600

    Begin basic spec setup for market

---
+++ spec/market_spec.rb
@@ -0,0 +1,13 @@
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+RSpec.describe Market do
+  before :each do 
+    @market = Market.new("South Pearl Street Farmers Market")
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+  end
